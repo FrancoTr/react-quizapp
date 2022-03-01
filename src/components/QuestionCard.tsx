b8ed60d290a0451ab9c2cb0eadcdca5a2804ddfa
Franco Traverso <franco-t20@outlook.com>
2022-02-28 22:49:58 -0300

    Change userAnswer type

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 
+// Types
+import { AnswerObject } from "../App";
+
 type Props = {
   question: string;
   answers: string[];
   callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
-  userAnswer: any;
+  userAnswer: AnswerObject | undefined;
   questionNr: number;
   totalQuestions: number;
 };
